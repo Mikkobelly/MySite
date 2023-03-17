a9a520d4f56177d3e0d937763ff262809b86159b
Mikkobelly <mikiakuta0529@gmail.com>
2023-03-17 00:37:57 +0000

    First commit

---
+++ js/app.js
@@ -0,0 +1,107 @@
+//Collapsible navigation
+(function (navigation) {
+    const menuIcon = document.querySelector('.dropdown__menu');
+    const closeIcon = document.querySelector('.dropdown__close');
+    const menuIconBox = document.querySelector('.dropdown__menu-box');
+    const navList = document.querySelector('.navigation-list');
+
+    menuIcon.addEventListener('click', () => {
+        navList.classList.add('isVisible');
+        menuIconBox.classList.add('hidden');
+    })
+
+    closeIcon.addEventListener('click', () => {
+        navList.classList.remove('isVisible');
+        menuIconBox.classList.remove('hidden');
+    })
+})();
+
+
+//Contact form validation
+(function form() {
+    const form = document.querySelector('.contact__form');
+    const nameInput = document.querySelector('input[name="name"]');
+    const emailInput = document.querySelector('input[name="email"]');
+    const telInput = document.querySelector('input[name="tel"]');
+
+
+    function showErrMsg(input, message) {
+        let container = input.parentElement;
+
+        //Remove an exisiting error
+        let error = container.querySelector('.error-message');
+        if (error) {
+            container.removeChild(error);
+        }
+
+        //Add the error if the message is not empty
+        if (message) {
+            let error = document.createElement('p');
+            error.classList.add('error-message');
+            error.innerText = message;
+            container.appendChild(error);
+        }
+    };
+
+
+    function validateNameInput() {
+        if (!nameInput.value) {
+            showErrMsg(nameInput, 'Name is a required filed');
+            return false;
+        }
+
+        showErrMsg(nameInput, null);
+        return true;
+    };
+
+
+    function validateEmailInput() {
+        if (!emailInput.value) {
+            showErrMsg(emailInput, 'Email is a required filed');
+            return false;
+        } else if (emailInput.value.indexOf('@') === -1
+            || emailInput.value.indexOf('.') === -1) {
+            showErrMsg(emailInput, 'Enter a valid email address');
+            return false;
+        }
+
+        showErrMsg(emailInput, null);
+        return true;
+    };
+
+
+    function validateTelInput() {
+        const parsedInput = Number(telInput.value);
+        if (isNaN(parsedInput)) {
+            showErrMsg(telInput, 'Enter a valid telephone number');
+            return false;
+        }
+
+        showErrMsg(telInput, null);
+        return true;
+    };
+
+
+    function validateForm() {
+        const isValidNameInput = validateNameInput();
+        const isValidEmailInput = validateEmailInput();
+        const isValidTelInput = validateTelInput();
+
+        return isValidNameInput && isValidEmailInput && isValidTelInput;
+    };
+
+
+    nameInput && nameInput.addEventListener('input', validateNameInput);
+    emailInput && emailInput.addEventListener('input', validateEmailInput);
+    telInput && telInput.addEventListener('input', validateTelInput);
+
+
+    form && form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        if (validateForm()) {
+            alert('Form submitted!');
+        } else {
+            alert('Please make adjustment to fullfill requirments')
+        }
+    });
+})();
